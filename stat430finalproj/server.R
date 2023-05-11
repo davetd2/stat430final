f781ad29a1be49a612e7086f678503e34a8bee91
davetd2 <55113228+davetd2@users.noreply.github.com>
2023-05-11 06:58:46 +0000

    finished up

--- stat430finalproj/server.R
+++ stat430finalproj/server.R
@@ -8,21 +8,48 @@
 #
 
 library(shiny)
+library(shinyjs)
+library(ggplot2)
+source("helper.R")
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
+  observeEvent(input$do, {
+    req(input$player)
+    updateSelectInput(inputId = "playersfound", choices = batting %>% filter(str_detect(name, input$player)) %>% select(name))
+  })
+#batting[grep(input$player, batting$name),]$name)
+  observeEvent(input$playersfound, {
+    req(input$playersfound)
+    if (input$playersfound == "NA") {
+      
+    } else{
+      updateSelectInput(inputId = "yearchosen", choices = 1:max((batting %>% filter(name == input$playersfound))$szn))
+    }
+  })
+  
+  output$simplot = renderPlot({
+    req(input$player, input$playersfound, input$yearchosen, input$graph)
+    ggplot(data = simchartdata(getPYID(input$playersfound, input$yearchosen), 
+                                        input$yearchosen,
+                                        battersimscore(getPYID(input$playersfound, input$yearchosen), input$yearchosen)),
+                    aes(x = szn, y = sim_score, color = name)) +
+      geom_line() + 
+      labs(x = "Season Number", y = "Similarity Score", title = "Similarity Score per season")
+  })
+  
+  output$warplot = renderPlot({
+    req(input$player, input$playersfound, input$yearchosen, input$graph)
+    ggplot(data = simchartdata(getPYID(input$playersfound, input$yearchosen), 
+                               input$yearchosen,
+                               battersimscore(getPYID(input$playersfound, input$yearchosen), input$yearchosen)),
+           aes(x = szn, y = totalWAR, color = name)) +
+      geom_line() + 
+      labs(x = "Season Number", y = "WAR per season", title = "WAR data per season")
+  })
+  
+  observeEvent(input$clear, {
+    refresh()
+  })
 
 }
--- stat430finalproj/ui.R
+++ stat430finalproj/ui.R
@@ -10,43 +10,46 @@
 library(shiny)
 library(shinythemes)
 library(Lahman)
-#source("helper.R")
+library(shinyjs)
+source("helper.R")
 
 # Define UI for application that draws a histogram
 
 fluidPage(
+  useShinyjs(),
   theme = shinytheme("cerulean"),
   sidebarLayout(
     sidebarPanel(
-      verticalLayout(
-        h3("Player(s):"),
-        textInput(
-          "player",
-          "Type a player ",
-          placeholder = "ex: 'Rodriguez'"
-        ),
-        radioButtons(
-          "bp",
-          label = "Batter or pitcher?",
-          choices = c("batting", "pitching")
-        ),
-      ),
+    verticalLayout(
       h3("Player(s):"),
-      selectInput("playersfound", label = "players found:", choices = c(NULL)),
-      selectInput("yearchosen", label = "season:", choices = NULL),
-      fluidRow(
-        actionButton("do", "Graph"),
-        actionButton("clear", "reset"),
+      textInput(
+        "player",
+        "Search players by name keyword:",
+        placeholder = "ex: 'Mark McGwire, Sammy Sosa'"
       ),
+    ),
+    fluidRow(
+      actionButton("do", "Search"),
+      actionButton("clear", "Reset"),
+    ),
+    h3("Player(s):"),
+              selectInput("playersfound", label = "players found:", choices = c("NA", "NA")),
+              selectInput("yearchosen", label = "season:", choices = c("NA", "NA")),
+    actionButton("graph", "Graph")
     ),
     
     mainPanel(tabsetPanel(
-      tabPanel("similarity score", "This page graphs the similarity scores of the selected player and the 10 most similar players based on the chosen season",
+      tabPanel("similarity score", "This page graphs the similarity scores of the selected player and the 5 most similar players based on the chosen season",
                mainPanel(plotOutput("simplot"),)),
       tabPanel("WAR", "This page graphs the career WAR trajectories of the selected player and the three most similar players based on similarity score",
                mainPanel(plotOutput("warplot")))
     ),
     ),
-  ),
-  
+    ),
+    
 )
+  
+  # Application title
+  
+  
+  # Sidebar with a slider input for number of bins
